799b1f325c898f7c9563b79bec9b7bcca37b9788
Arnaud Delorme <arnodelorme@gmail.com>
2023-01-02 12:47:44 -0800

    process all MEG files

--- matlab_process_meg.m
+++ matlab_process_meg.m
@@ -1,16 +1,142 @@
-[ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
-EEG = pop_fileio('/Users/arno/Downloads/Donders/sub-A2002/meg/sub-A2002_task-auditory_meg.ds', 'dataformat','auto');
-EEG = pop_resample( EEG, 120);
-EEG = pop_epoch( EEG, {  '14 Start File 006.wav'  '14 Start File 010.wav'  '14 Start File 012.wav'  '14 Start File 018.wav'  '14 Start File 021.wav'  '14 Start File 027.wav'  '14 Start File 028.wav'  '14 Start File 033.wav'  '14 Start File 036.wav'  '14 Start File 037.wav'  '14 Start File 043.wav'  '14 Start File 044.wav'  '14 Start File 046.wav'  '14 Start File 047.wav'  '14 Start File 049.wav'  '14 Start File 050.wav'  '14 Start File 063.wav'  '14 Start File 067.wav'  '14 Start File 069.wav'  '14 Start File 070.wav'  '14 Start File 075.wav'  '14 Start File 079.wav'  '14 Start File 086.wav'  '14 Start File 088.wav'  '14 Start File 090.wav'  '14 Start File 095.wav'  '14 Start File 098.wav'  '14 Start File 102.wav'  '14 Start File 109.wav'  '14 Start File 113.wav'  '14 Start File 119.wav'  '14 Start File 122.wav'  '14 Start File 124.wav'  '14 Start File 128.wav'  '14 Start File 130.wav'  '14 Start File 134.wav'  '14 Start File 142.wav'  '14 Start File 145.wav'  '14 Start File 148.wav'  '14 Start File 151.wav'  '14 Start File 152.wav'  '14 Start File 157.wav'  '14 Start File 158.wav'  '14 Start File 160.wav'  '14 Start File 162.wav'  '14 Start File 166.wav'  '14 Start File 170.wav'  '14 Start File 175.wav'  '14 Start File 184.wav'  '14 Start File 186.wav'  '14 Start File 190.wav'  '14 Start File 192.wav'  '14 Start File 195.wav'  '14 Start File 196.wav'  '14 Start File 198.wav'  '14 Start File 202.wav'  '14 Start File 203.wav'  '14 Start File 204.wav'  '14 Start File 206.wav'  '14 Start File 212.wav'  '14 Start File 213.wav'  '14 Start File 214.wav'  '14 Start File 221.wav'  '14 Start File 226.wav'  '14 Start File 235.wav'  '14 Start File 236.wav'  '14 Start File 237.wav'  '14 Start File 244.wav'  '14 Start File 247.wav'  '14 Start File 251.wav'  '14 Start File 252.wav'  '14 Start File 260.wav'  '14 Start File 261.wav'  '14 Start File 262.wav'  '14 Start File 264.wav'  '14 Start File 267.wav'  '14 Start File 277.wav'  '14 Start File 279.wav'  '14 Start File 281.wav'  '14 Start File 292.wav'  '14 Start File 294.wav'  '14 Start File 295.wav'  '14 Start File 297.wav'  '14 Start File 302.wav'  '14 Start File 305.wav'  '14 Start File 307.wav'  '14 Start File 310.wav'  '14 Start File 313.wav'  '14 Start File 315.wav'  '14 Start File 317.wav'  '14 Start File 318.wav'  '14 Start File 319.wav'  '14 Start File 321.wav'  '14 Start File 329.wav'  '14 Start File 333.wav'  '14 Start File 339.wav'  '14 Start File 342.wav'  '14 Start File 343.wav'  '14 Start File 344.wav'  '14 Start File 352.wav'  '14 Start File 357.wav'  '14 Start File 361.wav'  '14 Start File 364.wav'  '14 Start File 376.wav'  '14 Start File 385.wav'  '14 Start File 389.wav'  '14 Start File 390.wav'  '14 Start File 399.wav'  '14 Start File 401.wav'  '14 Start File 406.wav'  '14 Start File 501.wav'  '14 Start File 502.wav'  '14 Start File 504.wav'  '14 Start File 505.wav'  '14 Start File 507.wav'  '14 Start File 508.wav'  '14 Start File 514.wav'  '14 Start File 516.wav'  '14 Start File 522.wav'  '14 Start File 525.wav'  '14 Start File 530.wav'  '14 Start File 531.wav'  '14 Start File 532.wav'  '14 Start File 539.wav'  '14 Start File 553.wav'  '14 Start File 561.wav'  '14 Start File 562.wav'  '14 Start File 568.wav'  '14 Start File 571.wav'  '14 Start File 574.wav'  '14 Start File 577.wav'  '14 Start File 589.wav'  '14 Start File 592.wav'  '14 Start File 594.wav'  '14 Start File 597.wav'  '14 Start File 599.wav'  '14 Start File 608.wav'  '14 Start File 610.wav'  '14 Start File 611.wav'  '14 Start File 615.wav'  '14 Start File 616.wav'  '14 Start File 617.wav'  '14 Start File 618.wav'  '14 Start File 620.wav'  '14 Start File 621.wav'  '14 Start File 627.wav'  '14 Start File 629.wav'  '14 Start File 632.wav'  '14 Start File 635.wav'  '14 Start File 636.wav'  '14 Start File 637.wav'  '14 Start File 643.wav'  '14 Start File 650.wav'  '14 Start File 654.wav'  '14 Start File 659.wav'  '14 Start File 663.wav'  '14 Start File 667.wav'  '14 Start File 671.wav'  '14 Start File 672.wav'  '14 Start File 673.wav'  '14 Start File 674.wav'  '14 Start File 677.wav'  '14 Start File 681.wav'  '14 Start File 694.wav'  '14 Start File 700.wav'  '14 Start File 707.wav'  '14 Start File 710.wav'  '14 Start File 717.wav'  '14 Start File 720.wav'  '14 Start File 722.wav'  '14 Start File 723.wav'  '14 Start File 724.wav'  '14 Start File 729.wav'  '14 Start File 740.wav'  '14 Start File 745.wav'  '14 Start File 750.wav'  '14 Start File 754.wav'  '14 Start File 755.wav'  '14 Start File 756.wav'  '14 Start File 758.wav'  '14 Start File 759.wav'  '14 Start File 769.wav'  '14 Start File 772.wav'  '14 Start File 774.wav'  '14 Start File 776.wav'  '14 Start File 782.wav'  '14 Start File 785.wav'  '14 Start File 788.wav'  '14 Start File 799.wav'  '14 Start File 806.wav'  '14 Start File 814.wav'  '14 Start File 820.wav'  '14 Start File 823.wav'  '14 Start File 825.wav'  '14 Start File 827.wav'  '14 Start File 828.wav'  '14 Start File 830.wav'  '14 Start File 831.wav'  '14 Start File 834.wav'  '14 Start File 846.wav'  '14 Start File 848.wav'  '14 Start File 851.wav'  '14 Start File 853.wav'  '14 Start File 855.wav'  '14 Start File 856.wav'  '14 Start File 858.wav'  '14 Start File 865.wav'  '14 Start File 867.wav'  '14 Start File 870.wav'  '14 Start File 874.wav'  '14 Start File 882.wav'  '14 Start File 883.wav'  '14 Start File 884.wav'  '14 Start File 886.wav'  '14 Start File 888.wav'  '14 Start File 891.wav'  '14 Start File 893.wav'  '14 Start File 896.wav'  '14 Start File 897.wav'  '14 Start File 908.wav'  }, [0  3], 'epochinfo', 'yes');
-data = EEG.data;
-data = data(33:305,:,:); % select channels of interest
-save -mat data.mat data
+files = { ...
+    'sub-A2002/meg/sub-A2002_task-auditory_meg.ds';
+'sub-A2003/meg/sub-A2003_task-auditory_meg.ds';
+'sub-A2004/meg/sub-A2004_task-auditory_meg.ds';
+'sub-A2005/meg/sub-A2005_task-auditory_meg.ds';
+'sub-A2006/meg/sub-A2006_task-auditory_meg.ds';
+'sub-A2007/meg/sub-A2007_task-auditory_meg.ds';
+'sub-A2008/meg/sub-A2008_task-auditory_meg.ds';
+'sub-A2009/meg/sub-A2009_task-auditory_meg.ds';
+'sub-A2010/meg/sub-A2010_task-auditory_meg.ds';
+'sub-A2011/meg/sub-A2011_task-auditory_run-2_meg.ds';
+'sub-A2013/meg/sub-A2013_task-auditory_meg.ds';
+'sub-A2014/meg/sub-A2014_task-auditory_meg.ds';
+'sub-A2015/meg/sub-A2015_task-auditory_meg.ds';
+'sub-A2016/meg/sub-A2016_task-auditory_meg.ds';
+'sub-A2017/meg/sub-A2017_task-auditory_meg.ds';
+'sub-A2019/meg/sub-A2019_task-auditory_meg.ds';
+'sub-A2020/meg/sub-A2020_task-auditory_meg.ds';
+'sub-A2021/meg/sub-A2021_task-auditory_meg.ds';
+'sub-A2024/meg/sub-A2024_task-auditory_meg.ds';
+'sub-A2025/meg/sub-A2025_task-auditory_meg.ds';
+'sub-A2027/meg/sub-A2027_task-auditory_meg.ds';
+'sub-A2028/meg/sub-A2028_task-auditory_meg.ds';
+'sub-A2029/meg/sub-A2029_task-auditory_meg.ds';
+'sub-A2030/meg/sub-A2030_task-auditory_meg.ds';
+'sub-A2031/meg/sub-A2031_task-auditory_meg.ds';
+'sub-A2032/meg/sub-A2032_task-auditory_meg.ds';
+'sub-A2033/meg/sub-A2033_task-auditory_meg.ds';
+'sub-A2034/meg/sub-A2034_task-auditory_meg.ds';
+'sub-A2035/meg/sub-A2035_task-auditory_meg.ds';
+'sub-A2036/meg/sub-A2036_task-auditory_run-2_meg.ds';
+'sub-A2037/meg/sub-A2037_task-auditory_meg.ds';
+'sub-A2038/meg/sub-A2038_task-auditory_meg.ds';
+'sub-A2039/meg/sub-A2039_task-auditory_meg.ds';
+'sub-A2040/meg/sub-A2040_task-auditory_meg.ds';
+'sub-A2041/meg/sub-A2041_task-auditory_meg.ds';
+'sub-A2042/meg/sub-A2042_task-auditory_meg.ds';
+'sub-A2046/meg/sub-A2046_task-auditory_meg.ds';
+'sub-A2047/meg/sub-A2047_task-auditory_meg.ds';
+'sub-A2049/meg/sub-A2049_task-auditory_meg.ds';
+'sub-A2050/meg/sub-A2050_task-auditory_meg.ds';
+'sub-A2051/meg/sub-A2051_task-auditory_meg.ds';
+'sub-A2052/meg/sub-A2052_task-auditory_meg.ds';
+'sub-A2053/meg/sub-A2053_task-auditory_meg.ds';
+'sub-A2055/meg/sub-A2055_task-auditory_meg.ds';
+'sub-A2056/meg/sub-A2056_task-auditory_meg.ds';
+'sub-A2057/meg/sub-A2057_task-auditory_meg.ds';
+'sub-A2058/meg/sub-A2058_task-auditory_meg.ds';
+'sub-A2059/meg/sub-A2059_task-auditory_meg.ds';
+'sub-A2061/meg/sub-A2061_task-auditory_meg.ds';
+'sub-A2062/meg/sub-A2062_task-auditory_run-2_meg.ds';
+'sub-A2063/meg/sub-A2063_task-auditory_run-2_meg.ds';
+'sub-A2064/meg/sub-A2064_task-auditory_meg.ds';
+'sub-A2065/meg/sub-A2065_task-auditory_meg.ds';
+'sub-A2066/meg/sub-A2066_task-auditory_meg.ds';
+'sub-A2067/meg/sub-A2067_task-auditory_meg.ds';
+'sub-A2068/meg/sub-A2068_task-auditory_meg.ds';
+'sub-A2069/meg/sub-A2069_task-auditory_meg.ds';
+'sub-A2070/meg/sub-A2070_task-auditory_meg.ds';
+'sub-A2071/meg/sub-A2071_task-auditory_meg.ds';
+'sub-A2072/meg/sub-A2072_task-auditory_meg.ds';
+'sub-A2073/meg/sub-A2073_task-auditory_meg.ds';
+'sub-A2075/meg/sub-A2075_task-auditory_meg.ds';
+'sub-A2076/meg/sub-A2076_task-auditory_run-2_meg.ds';
+'sub-A2077/meg/sub-A2077_task-auditory_meg.ds';
+'sub-A2078/meg/sub-A2078_task-auditory_meg.ds';
+'sub-A2079/meg/sub-A2079_task-auditory_meg.ds';
+'sub-A2080/meg/sub-A2080_task-auditory_meg.ds';
+'sub-A2083/meg/sub-A2083_task-auditory_meg.ds';
+'sub-A2084/meg/sub-A2084_task-auditory_run-2_meg.ds';
+'sub-A2085/meg/sub-A2085_task-auditory_meg.ds';
+'sub-A2086/meg/sub-A2086_task-auditory_meg.ds';
+'sub-A2088/meg/sub-A2088_task-auditory_meg.ds';
+'sub-A2089/meg/sub-A2089_task-auditory_meg.ds';
+'sub-A2090/meg/sub-A2090_task-auditory_meg.ds';
+'sub-A2091/meg/sub-A2091_task-auditory_meg.ds';
+'sub-A2092/meg/sub-A2092_task-auditory_meg.ds';
+'sub-A2094/meg/sub-A2094_task-auditory_meg.ds';
+'sub-A2095/meg/sub-A2095_task-auditory_meg.ds';
+'sub-A2096/meg/sub-A2096_task-auditory_meg.ds';
+'sub-A2097/meg/sub-A2097_task-auditory_meg.ds';
+'sub-A2098/meg/sub-A2098_task-auditory_meg.ds';
+'sub-A2099/meg/sub-A2099_task-auditory_meg.ds';
+'sub-A2101/meg/sub-A2101_task-auditory_meg.ds'; 
+'sub-A2102/meg/sub-A2102_task-auditory_meg.ds'; 
+'sub-A2103/meg/sub-A2103_task-auditory_meg.ds';
+'sub-A2104/meg/sub-A2104_task-auditory_meg.ds'; 
+'sub-A2105/meg/sub-A2105_task-auditory_meg.ds'; 
+'sub-A2106/meg/sub-A2106_task-auditory_meg.ds'; 
+'sub-A2108/meg/sub-A2108_task-auditory_meg.ds'; 
+'sub-A2109/meg/sub-A2109_task-auditory_meg.ds';
+'sub-A2110/meg/sub-A2110_task-auditory_meg.ds'; 
+'sub-A2111/meg/sub-A2111_task-auditory_meg.ds'; 
+'sub-A2113/meg/sub-A2113_task-auditory_meg.ds'; 
+'sub-A2114/meg/sub-A2114_task-auditory_meg.ds'; 
+'sub-A2116/meg/sub-A2116_task-auditory_meg.ds'; 
+'sub-A2117/meg/sub-A2117_task-auditory_meg.ds'; 
+'sub-A2119/meg/sub-A2119_task-auditory_meg.ds'; 
+'sub-A2120/meg/sub-A2120_task-auditory_meg.ds'; 
+'sub-A2121/meg/sub-A2121_task-auditory_meg.ds'; 
+'sub-A2122/meg/sub-A2122_task-auditory_meg.ds'; 
+'sub-A2124/meg/sub-A2124_task-auditory_meg.ds'; 
+'sub-A2125/meg/sub-A2125_task-auditory_meg.ds' }; % last file cannot find type automatically, add ", 'headerformat', 'ctf_ds');" to line 146 of pop_fileio
 
-ind = [];
-for iEvent = 1:length(EEG.event)
-    if contains(EEG.event(iEvent).type, 'wav')
-        ind = [ ind iEvent];
+
+if ~exist('eeglab')
+    addpath('~/data/matlab/eeglab'); eeglab
+end
+
+filePath = '/Users/arno/Downloads/Donders/';
+for iFile = 1:length(files)
+    try
+        subject    = files{iFile}(1:9);
+        fileMat    = fullfile(filePath, [ subject '.mat' ]);
+        if ~exist(fileMat, "file")
+            subjectnum = str2num( files{iFile}(7:9) );
+            EEG = pop_fileio( fullfile(filePath, files{iFile}), 'dataformat','auto');
+            EEG = pop_select(EEG, 'chantype', 'meggrad');
+            EEG = eeg_epoch2continuous(EEG);
+            EEG = pop_epoch(EEG, 'wav', [0 3]);
+        
+            audioFile = cell(1, EEG.trials);
+            for iEvent = 1:length(EEG.event)
+                if contains(EEG.event(iEvent).type, '.wav')
+                    audioFile{ EEG.event(iEvent).epoch } = strip(EEG.event(iEvent).type(14:end));
+                end
+            end
+            if any(cellfun(@isempty, audioFile))
+                error('Some epochs do not contain data')
+            end
+            data = [];
+            data.data = EEG.data;
+            data.audiofiles = audioFile;
+            data.subject    = subject;
+            data.subjectnum = subjectnum;
+            save('-mat', fileMat, '-struct', 'data');
+        end
+        fprintf(2, '%d OK \n', iFile)
+    catch
+        fprintf(2, '\nFile error %s\n\n', files{iFile})
     end
 end
-dict = { EEG.event(ind).type }’
-save -mat data.mat data
