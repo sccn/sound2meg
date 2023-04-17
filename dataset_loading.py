19b3015e409c15af116ebb65df7e483f44932fe9
Muhammad Abdullah Khalid <48885205+makhalid1999@users.noreply.github.com>
2023-04-17 09:36:40 +0500

    Updates

--- dataset_loading.py
+++ dataset_loading.py
@@ -21,13 +21,12 @@ class Sound2MEGDataset(Dataset):
     self.subjects = []
     self.embedding_type = embedding_type
     if embedding_type == 'mel':
-      self.subpath = 'Mel_Embedding120/mel_'
+      self.subpath = 'arno/Mel_Embedding120/mel_'
     elif embedding_type == 'Wav2Vec':
-      self.subpath = 'Wav2Vec_Embeddings/'
-#     count = 0 # dtruong - unused
+      self.subpath = 'jiyul/wavEmbedding/'
     for subject in range(0, 126):
-      if os.path.exists(self.path + 'MEG_Signals/S%03dT000.npy'%subject):
-        files = glob.glob(self.path + 'MEG_Signals/S%03dT*.npy'%subject) 
+      if os.path.exists(self.path + 'arno/MEG_Signals/S%03dT000.npy'%subject):
+        files = glob.glob(self.path + 'arno/MEG_Signals/S%03dT*.npy'%subject) 
         self.sizes = np.append(self.sizes, len(files)) # dtruong - number of (brain) samples. So not the file size
         self.subjects.append( '%03d'%subject )
 #         count = count + 1 # dtruong - unused
@@ -36,19 +35,16 @@ class Sound2MEGDataset(Dataset):
   def __getitem__(self, idx):
     file_idx_upper_bounds = np.cumsum(self.sizes) # dutruong - inclusive lower bound
     idx_file = np.argwhere(file_idx_upper_bounds > idx)[0][0] # dutruong - first upper bound to contain idx (exclusive) is the file index. np.argwhere returns array (here of size 1), hence the 2nd 0 indexing
-#     for i in range(len(self.sizes)):              #Taking a cummulative sum of all the sizes
-#       if np.cumsum(self.sizes)[i] > idx:          #This way we will know the ranges of the different MEG Signal samples in the context of the entire dataset. # dutruong - to determine which elem of self.sizes the idx corresponds to. This means samples are consecutive. Have to make sure shuffling.
-#         idx_file = i                              #For example, say the sizes of the mat files are [3, 5, 2]
-#         break                                     #Using the cummulative function, we get [3, 8, 10]
-    #mat_file = loadmat(self.path + 'mat_files/' + self.filename[idx_file]) #This tells that samples 0 to 2 are in the first file, 3 to 7 in the second and so on...
+#Taking a cummulative sum of all the sizes
+#This way we will know the ranges of the different MEG Signal samples in the context of the entire dataset. # dutruong - to determine which elem of self.sizes the idx corresponds to. This means samples are consecutive. Have to make sure shuffling.
+#For example, say the sizes of the mat files are [3, 5, 2]
+#Using the cummulative function, we get [3, 8, 10]
     if idx_file == 0:                             #Accordingly, we see which sample is in which range by finding the smallest value in the cumsum larger than idx                           
       idx_sound = idx
     else:
-#       idx_sound = idx - np.cumsum(self.sizes)[idx_file - 1]
       idx_sound = idx - file_idx_upper_bounds[idx_file - 1]    
-
-    MEG_Signal = np.load(self.path + 'MEG_Signals/S'+self.subjects[idx_file]+'T%03d.npy'%idx_sound)
-    with open(self.path + 'MEG_Signals/audios.csv', 'r') as f:
+    MEG_Signal = np.load(self.path + 'arno/MEG_Signals/S'+self.subjects[idx_file]+'T%03d.npy'%idx_sound)
+    with open(self.path + 'arno/MEG_Signals/audios.csv', 'r') as f:
       audios = list(csv.reader(f))
       audio_file = audios[idx_file][idx_sound][2:5] # file name "['186.wav']"
     #Finding the associated audiofile, and extracting only the number of the file out of it
@@ -56,12 +52,9 @@ class Sound2MEGDataset(Dataset):
     #The filenames are in the format 'mel_<number>.npy' so importing accordingly
     Sound_Signal = np.load(self.path + self.subpath + audio_file + '.npy')
     if self.embedding_type == 'Wav2Vec':
-      Sound_Signal = np.transpose(Sound_Signal)
+      Sound_Signal = Sound_Signal[0]
     #mat_file = mat_file['data']
     MEG_Signal = np.float32(MEG_Signal[:273, :]) # dutruong - some dataset have more channels
-#     #Baseline Correction
-#     mean_5 = np.mean(MEG_Signal[:,:60], axis=1) # dutruong - ERROR: second baseline removal
-#     MEG_Signal = MEG_Signal - mean_5[:,None]
     #Robust Scaler
     scaler = RobustScaler().fit(MEG_Signal)
     MEG_Signal = scaler.transform(MEG_Signal)
